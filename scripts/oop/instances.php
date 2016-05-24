bc201b72857e94a2edcd0b7f08126db6445cb8cb
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-05-24 11:55:47 -0500

    updated file after magic methods

--- scripts/oop/instances.php
+++ scripts/oop/instances.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
+require_once "../../ini.php";
 require_once "./classes.php";
 
-$recipe1 = new Recipe();
+$recipe1 = new Recipe("a first example");
 
-$recipe2 = new Recipe();
-
-$recipe2->setTitle("a simple example");
+$recipe2 = new Recipe("a simple example");
 
 print $recipe2->displayRecipe();
+
+print $recipe2;
 ?>
