f3a47ae8e0e457e9815208add028f7bef69afb3a
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-05-24 16:25:13 -0500

    Added examples of inheritance and the scope resolution operator

--- scripts/oop/instances.php
+++ scripts/oop/instances.php
@@ -6,6 +6,10 @@ $recipe1 = new Recipe("a first example");
 
 $recipe2 = new Recipe("a simple example");
 
+//print $recipe2->displayRecipe();
+
+//print $recipe2;
+
 $objects = array();
 
 $objects[] = new Recipe("recipe one");
@@ -14,4 +18,41 @@ $objects[] = new Recipe("recipe two");
 foreach($objects as $object){
 	print $object->getTitle() . "\n";
 }
+
+
+class TestOne 
+{
+	public $title = "The first test \n";	
+}
+
+class TestTwo extends TestOne
+{
+	public $title = "The second test \n";	
+	public $example = "an example";
+	public function exampleFunc(){
+		print "Hey hey hey hey";	
+	}
+}
+
+class TestThree extends TestTwo
+{
+	public $title = "The third test \n";	
+	public function getThis(){
+		parent::exampleFunc();	
+	}
+	
+	public function __destruct(){
+		print "\n$this->title is being destroyed";	
+	}
+}
+
+$objectOne = new TestOne();
+$objectTwo = new TestTwo();
+$objectThree = new TestThree();
+
+print $objectOne->title;
+print $objectTwo->title;
+print $objectThree->title;
+$objectThree->getThis();
+
 ?>
