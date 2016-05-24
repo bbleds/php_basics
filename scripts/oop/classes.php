fa9cf283967d2f49e53544c246ec173e92f202c2
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-05-24 09:25:19 -0500

    cleaning up class file

--- scripts/oop/classes.php
+++ scripts/oop/classes.php
@@ -4,24 +4,36 @@ require_once "../../ini.php";
 
 class Recipe 
 {
-	public $title;
+	private $title;
 	public $ingredients = array();
 	public $instructions = array();
 	public $yield;
 	public $tag = array();
 	public $source = 'treehouse';
 
+	public function addIngredients($item, $amount = null, $measure = null)
+	{
+		$this->ingredients[] = array(
+			'item' => $item,
+			'amount' => $amount,
+			'measure' => $measure
+		);
+	}
+
 	public function displayRecipe()
 	{
 		return $this->title . " by " . $this->source;
 	}
+
+	public function setTitle($title)
+	{
+		$this->title = ucwords($title);
+	}
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
 }
 
-$recipe1 = new Recipe();
-
-$recipe2 = new Recipe();
-
-$recipe2->title = "An example";
-
-print $recipe2->displayRecipe();
 ?>
