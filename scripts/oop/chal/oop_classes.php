cb7c4a730a1c77da85d92170fa4dec84450cb6ce
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-05-25 08:21:22 -0500

    Added basic getter and setter for name

--- scripts/oop/chal/oop_classes.php
+++ scripts/oop/chal/oop_classes.php
@@ -1,1 +1,61 @@
+<?php
 
+/**
+ * @author Ben Bledsoe
+ * @copyright 2016, The A Group
+ *
+ * Holds personal information and address for a specific person
+ */
+class Person 
+{
+	private $first_name;	
+	private $last_name;	
+	private $email;	
+	private $phone;	
+	private $address = array();
+	
+	/**
+	 * Person::get_name()
+	 *
+	 * Prints the $first_name and $last_name class properties
+	 *
+	 * @access private
+	 *
+	 * @return void
+	 */
+	public function get_name(){
+		print $this->first_name . " " . $this->last_name;	
+	}
+	
+	/**
+	 * Person::set_name()
+	 *
+	 * Sets the $first_name and $last_name class properties
+	 *
+	 * @access private
+	 *
+	 * @param string $firstName
+	 * @param string $lastName
+	 *
+	 * @return void
+	 */
+	public function set_name($firstName, $lastName){
+		$this->first_name = $firstName;
+		$this->last_name = $lastName;
+	}
+	
+	private function get_email(){
+		print $this->email;	
+	}
+	
+	private function get_phone(){
+		print $this->phone;	
+	}
+}
+
+$example = new Person();
+
+$example->set_name("Ben", "Bledsoe");
+$example->get_name();
+
+?>
