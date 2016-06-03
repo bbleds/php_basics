0f559d51f40d9a4fab25e497a35220c8ae26a4a0
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-06-03 09:25:33 -0500

    Init staff class

---
+++ scripts/summary/class.Staff.php
@@ -0,0 +1,20 @@
+<?php 
+
+define('SITES_PATH', realpath($_SERVER['DOCUMENT_ROOT']));
+
+require_once SITES_PATH.'/oop/class.Person.php';
+
+/**
+ * @author Ben Bledsoe
+ * @copyright 2016, The A Group
+ *
+ * Provides methods for ????????????????????????????
+ */
+class Staff extends Person 
+{
+	
+}
+
+print "Hello world";
+
+?>
