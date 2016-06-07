91e4744d25a4229a3cc084d38811e38f07d4637f
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-06-07 16:10:09 -0500

    updated class to work with sessions

--- scripts/summary/class.Staff.php
+++ scripts/summary/class.Staff.php
@@ -3,18 +3,314 @@
 define('SITES_PATH', realpath($_SERVER['DOCUMENT_ROOT']));
 
 require_once SITES_PATH.'/oop/class.Person.php';
+require_once SITES_PATH.'/tt4lib/src/class.MDB.php';
 
 /**
  * @author Ben Bledsoe
  * @copyright 2016, The A Group
  *
- * Provides methods for ????????????????????????????
+ * Extends person class with properties and methods for managing summary_staff records, admin module for managing staff
  */
 class Staff extends Person 
 {
 	
+	protected $dbCollectionName = 'summary_staff';
+	public $socialSites = array('blog','twitter','facebook','linkedIn','google plus', 'snapchat');
+	public $contact_methods = array('email', 'phone');
+	public $statuses = array('active', 'inactive');
+	
+	/**
+	 * Staff::output_social_fields()
+	 *
+	 * Outputs a prefilled input for each social site that user saved and an empty input for each social site user has not saved
+	 *
+	 * @access public
+	 * @access public
+	 *
+	 * @param array $sites -- the sites to check for
+	 * @param bool $recordExists
+	 * @param array $data -- record in db
+	 *
+	 * @return void
+	 */
+	 public function output_social_fields($sites, $recordExists, $data=array()){
+		
+		if(!$recordExists){
+			
+			// print input field for each site is $socialSite
+			foreach($sites as $site){
+				print '<label>' . ucwords($site) .'</label>';
+				print '<input type="url" class="form-control" name="social[' . $site .']" value="'.$this->getSubmittedField('post_data','social', $site).'"/>';
+			}
+			
+		}	else {
+			// store all sites user has entered
+			$enteredSites = array(); 
+	
+			foreach($sites as $site){
+				
+				for($i = 0; $i < count($data['social']); $i++){
+					if($data['social'][$i]['name'] == $site){
+						print '<label>' .ucwords($site) .'</label>';
+						print '<input type="url" class="form-control" name="social['. $site  .']" value="'.$data['social'][$i]['link'] .'"/>';
+						$enteredSites[] =  $data['social'][$i]['name'];
+						break;
+					}
+				}				
+			}
+			
+			// find the difference between the two arrays, and output an empty field for those that user has not entered 
+			foreach(array_diff($sites, $enteredSites) as $diff){
+				print '<label>' . ucwords($diff) .'</label>';
+				print '<input type="url" class="form-control" name="social[' . $diff .']" />';
+			}     
+		}
+	    
+	}
+	
+	/**
+	 * Staff::output_staff_status()
+	 *
+	 * Outputs radio button inputs, appropriate button is checked based on user input
+	 *
+	 * @access public
+	 *
+	 * @param string $statuses
+	 * @param bool $recordExists
+	 * @param array $data
+	 *
+	 * @return void
+	 */
+	 public function output_staff_statuses($statuses, $recordExists, $data=array()){
+	 	
+	 	print "<ul>";
+	 	
+	 	if(!$recordExists){
+	 		
+	 		foreach($statuses as $status){
+	 			print '<li><label><input type="radio" name="status" value="' . $status.'"';
+	 			
+	 			// if status exists on session
+	 			if($_SESSION['post_data']['status']){
+	 				// if value is value of current status
+	 				if($_SESSION['post_data']['status'] == $status){
+	 					print "checked";
+	 				}
+	 			}
+	 			
+	 			print '/>'. ucwords($status) .'</label></li>';	
+	 		}
+	 		
+	 	} else {
+		
+			$existingStatus = array($data['status']);
+		
+			// print current status
+			print '<li><label><input type="radio" name="status" value="' . $data['status'] . '" checked />'. ucwords($data['status']) .'</label><li>';
+		
+			$diffItems = array_diff($statuses, $existingStatus); 
+			foreach($diffItems as $diff){
+				print '<li><label><input type="radio" name="status" value="' . $diff . '"/>'. ucwords($diff) .'</label><li>';
+			}
+		}
+		
+		print "</ul>";
+	}
+	
+	/**
+	 * Staff::output_contact_methods()
+	 *
+	 * Outputs a checkbox for each item in $contact_methods, checkbox is checked if it exists on user
+	 *
+	 * @access public
+	 *
+	 * @param string $contact_methods
+	 * @param bool $recordExists
+	 * @param array $data
+	 *
+	 * @return void
+	 */
+	 public function output_contact_methods($contact_methods, $recordExists, $data=array()){
+	
+		print '<ul>';
+
+		if(!$recordExists){
+			
+			foreach($contact_methods as $method){
+				print '<li><label><input type="checkbox" name="contact_methods[]" value="' . $method . '"';
+				
+				// if contact_methods exist on session
+				if($_SESSION['post_data']['contact_methods']){
+					
+					// loop contact methods saved in session and output checked for any that match current $method
+					foreach($_SESSION['post_data']['contact_methods'] as $submittedMethod){
+						if($submittedMethod == $method){
+							print 'checked';
+						}
+					}
+				}
+				
+				print  '/>'. ucwords($method) .'</label></li>';
+			}
+		} else {
+	
+			// print each selected item
+		 	foreach($data['contact_methods'] as $method){
+		 		print '<li><label><input type="checkbox" name="contact_methods[]" value="' . $method . '" checked />'. ucwords($method) .'</label></li>';
+		 	}
+		 	
+		 	// print each unselected item
+		 	$diffItems = array_diff($contact_methods, $data['contact_methods']);
+		 	foreach($diffItems as $diff){
+		 		print '<li><label><input type="checkbox" name="contact_methods[]" value="' . $diff . '" />'. ucwords($diff) .'</label></li>';
+		 	}
+		}
+		
+		print '</ul>';
+	}
+	
+	/**
+	 * Staff::get_existing_general_fields()
+	 *
+	 * Returns a field value if it exists within a certain data set or empty string if it does not exist
+	 *
+	 * @access public
+	 *
+	 * @param array $data
+	 * @param string $fieldname
+	 *
+	 * @return string $value
+	 */
+	 public function get_existing_general_field($data, $fieldName){
+		
+		if( !isset($data[$fieldName]) || empty($data[$fieldName]) ){
+			$value = '';
+		} else {
+			$value = $data[$fieldName];
+		}
+	
+		return $value;	
+	}
+	
+	/**
+	 * Staff::build_staff_record_array()
+	 *
+	 * Returns an array that can be used to post new staff records to db or update existing members in db
+	 *
+	 * @access public
+	 *
+	 * @param array $data
+	 *
+	 * @return array $newRecord
+	 */
+	 public function build_staff_record_array($data){
+		// build new record to be inserted into db
+		$newRecord = array();
+		$newRecord['date_posted'] = new MongoDate();
+		
+		// key names in $_POST that should be pushed into $newRecord immediately
+		$immediateKeysToPush = array('email', 'phone', 'position', 'bio', 'status', 'contact_methods');
+		
+		
+		// loop through $immediateKeysToPush and push into $newRecord array
+		foreach($immediateKeysToPush as $key){
+			if(isset($data[$key]) && !empty($data[$key])){
+				$newRecord[$key] = $data[$key];
+			}
+		}
+		
+		// for each item in name array, push key value pair into the $newRecord array
+		foreach($data['name'] as $k=>$v){
+			$newRecord[$k] = $v;
+		}  
+		
+		//build address array
+		$address = $data['address'];
+		$address['state'] = $data['state'];
+		
+		foreach($address as $key=>$value){
+			if(!empty($address[$key])){
+				$newRecord['address'][$key] = $address[$key];
+			}
+		}
+		
+		//build social array
+		foreach($data['social'] as $k=>$v){
+			if(!empty($data['social'][$k])){
+				$newRecord['social'][] = array('name'=>$k,'link'=>$v);
+			}
+		}
+		
+		return $newRecord;
+	}
+
+	public function validate_required_string_fields(){
+		//place code here to run	
+	}
+	
+	/**
+	 * Staff::validate_mongo_id()
+	 *
+	 * Validate that an id exists and is a valid MongoId object
+	 *
+	 * @access public
+	 * 
+	 * @param string|object $id
+	 *
+	 * @return object|bool $id
+	 */
+	public function validate_mongo_id($id){
+		if( empty($id) ){
+			return false;
+		}
+		
+		if( !is_object($id) ){
+			try {
+				$id = new MongoId($id);		
+			}	catch (MongoException $e) {
+				return false;
+			}
+		}
+		
+		return $id;
+	}
+	
+	/**
+	 * Staff::getSumbittedField()
+	 *
+	 * returns the value of a field that has been submitted, allows for triple nested data
+	 *
+	 * @access public
+	 * 
+	 * @param string $topLevel
+	 * @param string $midLevel
+	 * @param string $bottomLevel
+	 *
+	 * @return string $fieldValue
+	 */
+	public function getSubmittedField($topLevel, $midLevel='', $bottomLevel=''){
+		if(empty($_SESSION[$topLevel])){
+			return '';
+		}
+		
+		$fieldValue='';
+		
+		if( empty($midLevel) && empty($bottomLevel) ){
+			$fieldValue = $_SESSION[$topLevel];
+		}
+		
+		if( empty($bottomLevel) && !empty($midLevel) ){
+			$fieldValue = $_SESSION[$topLevel][$midLevel];
+		}
+		
+		if( !empty($bottomLevel) && !empty($midLevel) ){
+			$fieldValue = $_SESSION[$topLevel][$midLevel][$bottomLevel];
+		}
+	
+		return $fieldValue;
+	}
+
 }
 
-print "Hello world";
-
+MDB::connect('bentt4');
 ?>
