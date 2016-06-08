30aca98c649ebea5cd9ef77cc2cade073dfd57e7
Benjamin Bledsoe <bledsoedesigns@gmail.com>
2016-06-07 19:58:00 -0500

    Update edit.php

--- scripts/summary/pages/edit.php
+++ scripts/summary/pages/edit.php
@@ -6,12 +6,25 @@ define('ROOT_PATH', realpath($_SERVER['DOCUMENT_ROOT']));
 require_once ROOT_PATH.'/summary/includes/class.Staff.php';
 require_once ROOT_PATH.'/tt4lib/src/class.Util.php';
 
+// check for empty or unspecified id
+if(	!isset($_GET['id']) || empty($_GET['id']) ){
+	die('There was an error retrieving that page, invalid ID, please try again! <br/><a href="../index.php">Go Back!</a>');
+}
+
+session_start();
+
 // set content to HTML, class.Person.php requires ini file
 header('Content-Type: text/html');
 
 $id = $_GET['id'];
 $staff = new Staff();
 $resp = $staff->find_one($id);
+
+// die if no results/ bad id
+if($resp['error']){
+	die('There was an error retrieving that page, please try again! <br/><a href="../index.php">Go Back!</a>');
+}
+
 $staffMember = $resp['data']['rows'][0];
 		
 ?>
@@ -38,7 +51,33 @@ $staffMember = $resp['data']['rows'][0];
 			<a href="create.php"><button class="btn btn-block">Add</button></a>
 		</div>
 		<div class="col-md-8">
-			<form method="post" action="../scripts/update_handler.php">
+
+		<div id="error_box" class="alert-danger">
+<?php
+
+if(isset($_SESSION['error_msg']) && !empty($_SESSION['error_msg']) ){
+
+	print 'There was an error!';
+	foreach($_SESSION['error_msg'] as $error){
+		print '<p>'.$error.'</p>';
+	}
+}
+
+
+die(print_r($_SESSION['post_data']));
+
+function getSubmitOrExistingField($staffData, $topField, $midField, $bottomField){
+// get session data
+$staff->getSubmittedField();
+	
+// else
+// get existing general field 
+}
+
+?>
+		</div>			
+
+			<form method="post" onsubmit="return validateForm()" action="../scripts/update_handler.php">
 				<h3>General</h3>
 				
 				<label for="first_name">First Name * </label>
@@ -48,16 +87,16 @@ $staffMember = $resp['data']['rows'][0];
 				<input type="text" class="form-control required" name="name[last_name]" id="last_name" value="<?php print $staff->get_existing_general_field($staffMember, 'last_name') ?>"/>
 
 				<label for="email">Email * </label>
-				<input type="text" class="form-control required" name="email" id="email" value="<?php print $staff->get_existing_general_field($staffMember, 'email') ?>"/>
+				<input type="email" class="form-control required" name="email" id="email" value="<?php print $staff->get_existing_general_field($staffMember, 'email') ?>"/>
 
 				<label for="phone">Phone Number</label>
 				<input type="text" class="form-control" name="phone" id="phone" value="<?php print $staff->get_existing_general_field($staffMember, 'phone') ?>"/>
 
 				<label for="position">Position</label>
 				<input type="text" class="form-control" name="position" id="position" value="<?php print $staff->get_existing_general_field($staffMember, 'position') ?>"/>
-
-				<label for="bio">Bio</label>
-				<input type="text" class="form-control" name="bio" id="bio" value="<?php print $staff->get_existing_general_field($staffMember, 'bio') ?>"/>
+				
+				<label>Bio</label>
+				<textarea class="form-control" name="bio"><?php print $staff->get_existing_general_field($staffMember, 'bio') ?></textarea>
 				
 				<h3>Status</h3>
 <?php 
@@ -117,15 +156,15 @@ foreach($states as $state){
 				<input type="num" class="form-control" name="address[zip]" id="zip" value="<?php print $zip ?>"/>
 				<input type="hidden" name="id" value="<?php print $_GET['id'] ?>" />
 				
-				<button type="submit" class="btn btn-primary">Udate Member</button>
+				<button type="submit" class="btn btn-primary">Update Member</button>
 			</form>
 		</div>
 	</div>
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	
-	<!-- custom js AJAX handler can simply redirect for now -->
+	<script type="text/javascript" src="../../tt4lib/js/email_check.js"></script>	
+	<script type="text/javascript" src="../scripts/client_validate.js"></script>	
 	
 	
 </body>
